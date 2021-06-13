f91c3bc1d7035d662203b8078c8d9150cfc18e0e
KIMJINOH97 <rlawlsdh1127@naver.com>
2021-06-13 17:56:17 +0900

    feat: String을 이용한 테스트 (테스트 작성연습 + 에러 처리 학습)

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,13 +1,39 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
     @Test
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+
+    @Test
+    void split(){
+        String alphabet = "a,b,c";
+        String[] split = alphabet.split(",");
+        assertThat(split).contains("a","b","c");
+        assertThat(split).containsExactly("a","b","c");
+    }
+
+    @Test
+    void substring(){
+        String actual = "(1,2)".substring(1,4);
+        assertThat(actual).isEqualTo("1,2");
+    }
+
+    @DisplayName("charAt()했을 때 범위를 벗어났을 때")
+    @Test
+    void charAt(){
+        String charTest = "abcdef";
+        char selectOne = charTest.charAt(2); // 'c' 뽑기
+        assertThat(selectOne).isEqualTo('c');
+        assertThatThrownBy(() -> charTest.charAt(6)).isInstanceOf(StringIndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: 6");
     }
 }
